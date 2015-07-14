29acb42e4f367529a69cd2d40ccf46a2a278eaf3
TienBao <ngtienbao@gmail.com>
2015-07-14 15:06:30 +0700

    change main.h

--- test.m
+++ test.m
@@ -1,16 +1,1 @@
-//
-//  main.m
-//  Flashlight
-//
-//  Created by Tien Bao Nguyen on 6/25/15.
-//  Copyright (c) 2015 TSO. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
-
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
-}
+TA LA BAO
